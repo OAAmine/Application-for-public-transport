ed8152ec2b39f7bd53545c2d328326c28bb63aa3
OULD AMARA Amine <ouldamaraamine@gmail.com>
2021-12-31 17:59:05 +0100

    added history

--- cartes.py
+++ cartes.py
@@ -33,14 +33,11 @@ class MainWindow(QMainWindow):
 
         self.webView = myWebView()
 
-
         controls_panel = QtWidgets.QGridLayout()
         mysplit = QSplitter(Qt.Vertical)
 
         # adding the map
         mysplit.addWidget(self.webView)
-
-
 
         # create the USER dropdown box
         _label_user = QLabel('User: ', self)
@@ -50,27 +47,20 @@ class MainWindow(QMainWindow):
         self.user_box.completer().setCompletionMode(QCompleter.PopupCompletion)
         self.user_box.setInsertPolicy(QComboBox.NoInsert)
 
-
         # create the ADD USER button
         self.add_user_button = QPushButton("Add user")
         # self.add_user_button.setGeometry(4, 4, 4, 4)
         self.add_user_button.clicked.connect(self.add_user)
 
-       
-       
         # create the delete history button
         self.delete_history_button = QPushButton("Delete history")
         # self.delete_history_button.setGeometry(4, 4, 4, 4)
-        self.delete_history_button.clicked.connect(self.delete_history)        
-        
-        
+        self.delete_history_button.clicked.connect(self.delete_history)
+
         # create the show history button
         self.show_history_button = QPushButton("Show history")
         # self.show_history_button.setGeometry(4, 4, 4, 4)
         self.show_history_button.clicked.connect(self.show_history)
-
-
-
 
         # create the Go button
         self.go_button = QPushButton("Find \n a \n way !")
@@ -81,92 +71,75 @@ class MainWindow(QMainWindow):
         _label_from = QLabel('From: ', self)
         _label_from.setFixedSize(40, 20)
         self.from_box = QComboBox()
-        self.from_box.setFixedSize(250,25)
+        self.from_box.setFixedSize(250, 25)
         self.from_box.setEditable(True)
         self.from_box.completer().setCompletionMode(QCompleter.PopupCompletion)
         self.from_box.setInsertPolicy(QComboBox.NoInsert)
-
 
         # create the TO dropdown box
         _label_to = QLabel('To: ', self)
         _label_to.setFixedSize(25, 20)
         self.to_box = QComboBox()
-        self.to_box.setFixedSize(250,25)
+        self.to_box.setFixedSize(250, 25)
         self.to_box.setEditable(True)
         self.to_box.completer().setCompletionMode(QCompleter.PopupCompletion)
         self.to_box.setInsertPolicy(QComboBox.NoInsert)
-
-
 
         # create the results table widget
         self.tableWidget = QTableWidget()
         self.tableWidget.doubleClicked.connect(self.table_Click)
         self.rows = []
-        
-        
-        
-        ###################"###################"###################""###################"###################"
-        #Laying out the previously created widgets
-        ###################"###################"###################"###################"###################"###################"
-        
 
-
+        # "###################"###################""###################"###################"
+        # Laying out the previously created widgets
+        # "###################"###################"###################"###################"###################"
 
         ############################################# ROW 1 ############################################################
-        #User
+        # User
         controls_panel.addWidget(
             _label_user, 1, 0, QtCore.Qt.AlignmentFlag.AlignRight)
-        controls_panel.addWidget(self.user_box, 1, 1, 
+        controls_panel.addWidget(self.user_box, 1, 1,
                                  QtCore.Qt.AlignmentFlag.AlignLeft)
-    
-        #Show history
+
+        # Show history
         controls_panel.addWidget(self.show_history_button, 1, 2,
                                  QtCore.Qt.AlignmentFlag.AlignLeft)
-        
-        
-        #Delete history
+
+        # Delete history
         controls_panel.addWidget(self.delete_history_button, 1, 3,
                                  QtCore.Qt.AlignmentFlag.AlignLeft)
 
-        #Add user
+        # Add user
         controls_panel.addWidget(self.add_user_button, 1, 4,
                                  QtCore.Qt.AlignmentFlag.AlignLeft)
-        
-        
+
         ############################################# ROW 2 ############################################################
 
-        #From
+        # From
         controls_panel.addWidget(
             _label_from, 2, 0, QtCore.Qt.AlignmentFlag.AlignRight)
         controls_panel.addWidget(
             self.from_box, 2, 1, QtCore.Qt.AlignmentFlag.AlignLeft)
 
-
-
-
         ############################################# ROW 3 ############################################################
 
-        #To
+        # To
         controls_panel.addWidget(
             _label_to, 3, 0, QtCore.Qt.AlignmentFlag.AlignRight)
         controls_panel.addWidget(
             self.to_box, 3, 1, QtCore.Qt.AlignmentFlag.AlignLeft)
 
-        #Go button
-        controls_panel.addWidget(self.go_button,2, 2, 2, 1)
-        
+        # Go button
+        controls_panel.addWidget(self.go_button, 2, 2, 2, 1)
 
         ############################################# ROW 4 ############################################################
 
-        #Table 
+        # Table
         controls_panel.addWidget(self.tableWidget, 4, 0, 1, 5)
 
-        
-        
         main.layout().addLayout(controls_panel)
         main.layout().addWidget(mysplit)
-        
-        
+
         # create the HOPS dropdown box
         # _label = QLabel('Hops: ', self)
         # _label.setFixedSize(20,20)
@@ -187,14 +160,11 @@ class MainWindow(QMainWindow):
         # controls_panel.addWidget(self.clear_button)
 
         # Add Folium map types options
-        # self.maptype_box = QComboBox()
-        # self.maptype_box.addItems(self.webView.maptypes)
-        # self.maptype_box.currentIndexChanged.connect(self.webView.setMap)
+        self.maptype_box = QComboBox()
+        self.maptype_box.addItems(self.webView.maptypes)
+        self.maptype_box.currentIndexChanged.connect(self.webView.setMap)
         # controls_panel.addWidget(self.maptype_box)
 
-        
-        
-        
         # opening window in maximized size
         self.showMaximized()
 
@@ -205,24 +175,24 @@ class MainWindow(QMainWindow):
         self.startingpoint = True
         self.show()
 
+    # show users in the list
 
-    #show users in the list
     def show_users(self):
         self.cursor.execute(
-             """SELECT name FROM users ORDER BY name""")
+            """SELECT name FROM users ORDER BY name""")
         self.conn.commit()
         rows_name = self.cursor.fetchall()
 
         for row in rows_name:
-            self.user_box.addItem(str(row[0]))  
-    
-    
-    #connect to the Data Base
+            self.user_box.addItem(str(row[0]))
+
+    # connect to the Data Base
+
     def connect_DB(self):
         self.conn = psycopg2.connect(
             database="metro", user="postgres", host="localhost", password="password")
-        
-    #show the stations in the list
+
+    # show the stations in the list
     def show_stations_in_lists(self):
         self.cursor = self.conn.cursor()
         self.cursor.execute(
@@ -232,10 +202,6 @@ class MainWindow(QMainWindow):
         for row in rows:
             self.from_box.addItem(str(row[0]))
             self.to_box.addItem(str(row[0]))
-
-
-      
-
 
     def table_Click(self):
         k = 0
@@ -253,44 +219,106 @@ class MainWindow(QMainWindow):
 
                 self.webView.addMarker(lat, lon)
             k = k + 1
+        # showing each station and the time it takes
+        # clear table
+        # while (self.tableWidget.rowCount() > 0) :
+            #  self.tableWidget.removeRow(0)
 
+        # self.tableWidget.setRowCount(0)
+        # self.tableWidget.setColumnCount(0)
 
-    #just a simple dialog box that asks for the name, maybe we'll add home address ? to be discussed if we still have time 
+        # show time in each transport type
+        # i = 0
+        # j = 0
+        # p = 0
+        # for col in self.rows[self.tableWidget.currentRow()]:
+        #     if j % 3 == 0:
+        #         p = p + 1
+        #     # if j-p == 3:
+        #     #     i = i + 1
+        #     #     j = 0
+        #     else:
+        #         if j + 1 - p == 4 and j == 5:
+        #             i = i + 1
+        #             j = 0
+        #             p = 1
+        #         self.tableWidget.setItem(
+        #             i, j + 1 - p, QTableWidgetItem(str(col)))
+        #     j = j + 1
+        self.tableWidget.clear()
+        i = 0
+        j = 0
+        p = 0
+        for col in self.rows[self.tableWidget.currentRow()]:
+            if j % 3 == 0:
+                j = j + 1
+            # if j-p == 3:
+            #     i = i + 1
+            #     j = 0
+            else:
+                if j == 4 :
+                    self.tableWidget.setItem(
+                    i, j - 1 , QTableWidgetItem(str(col)))
+                    i = i + 1
+                    j = 1
+                    self.tableWidget.setItem(
+                    i, j , QTableWidgetItem(str(col)))
+                    j = j + 1
+
+                else :        
+                    self.tableWidget.setItem(
+                        i, j , QTableWidgetItem(str(col)))
+                    j = j + 1
+        _current_user = str(self.user_box.currentText())
+        if _current_user != "anonymous" :
+
+            self.cursor.execute(
+            f"""SELECT id FROM users WHERE name = '{_current_user}'""")
+            self.conn.commit()
+            _current_user_id = self.cursor.fetchall()
+               
+            m = str(self.rows[self.tableWidget.currentRow()])
+            m2 = m.replace("'","$")
+            itineraire = m2.replace(",","£")
+
+            print(itineraire)
+            self.cursor.execute(
+                f"""INSERT INTO user_history VALUES ('{_current_user_id[0][0]}','{itineraire}')""")
+            self.conn.commit()
+            
+        
+
+    # just a simple dialog box that asks for the name, maybe we'll add home address ? to be discussed if we still have time
     def add_user(self):
         name, done1 = QtWidgets.QInputDialog.getText(
-             self, 'Input Dialog', 'Enter your name:')
+            self, 'Input Dialog', 'Enter your name:')
 
-        if done1 :
+        if done1:
             self.cursor.execute(
-             f"""INSERT INTO users(name) VALUES ('{name}')""")
+                f"""INSERT INTO users(name) VALUES ('{name}')""")
             self.conn.commit()
             self.user_box.addItem(f"""{name}""")
 
-
     def show_history(self):
-        #clear map
+        # clear map
         self.webView.clearMap(self.maptype_box.currentIndex())
         self.startingpoint = True
         self.update()
-        #clear table
-        while (self.tableWidget.rowCount() > 0) :
-            self.tableWidget.removeRow(0)
-        #complete code => get history from user history table 
-        
-        
+        # clear table
+        self.tableWidget.clear()
+
+        # complete code => get history from user history table
+
     def delete_history(self):
-        #remove all rows for a certain user
-        return  
-
-
-
+        # remove all rows for a certain user
+        return
 
     def button_Go(self):
         self.tableWidget.clearContents()
 
         _fromstation = str(self.from_box.currentText())
         _tostation = str(self.to_box.currentText())
-        _hops = 10 #int(self.hop_box.currentText())
+        _hops = 10  # int(self.hop_box.currentText())
 
         self.rows = []
 
@@ -319,20 +347,44 @@ class MainWindow(QMainWindow):
 
         numrows = len(self.rows)
         numcols = len(self.rows[-1]) - math.floor(len(self.rows[-1]) / 3.0) - 1
+        # print("type(self.rows)" + str(type(self.rows)))
+        # print(self.rows)
+        # print("len(self.rows) "+ str(len(self.rows)))
+        # print("len(self.rows[-1]) = " + str(len(self.rows[-1])))
+        # print("numrows = " + str(numrows))
+        # print("numcols = " + str(numcols))
         self.tableWidget.setRowCount(numrows)
         self.tableWidget.setColumnCount(numcols)
 
         i = 0
         for row in self.rows:
-            j = 0
+            j = 1
             k = 0
             for col in row:
-                if j % 3 == 0:
-                    k = k + 1
-                else:
-                    self.tableWidget.setItem(
-                        i, j-k, QTableWidgetItem(str(col)))
-                j = j + 1
+                # if j % 3 == 0:
+                if len(row) == 5:
+                    if col == row[0] or col == row[1] or col == row[3] or col == row[4]:
+                        k = k + 1
+                    else:
+                        self.tableWidget.setItem(
+                            i, j-k, QTableWidgetItem(str(col)))
+                    j = j + 1
+
+                if len(row) == 8:
+                    if col == row[0] or col == row[1] or col == row[3] or col == row[4] or col == row[6] or col == row[7]:
+                        k = k + 1
+                    else:
+                        self.tableWidget.setItem(
+                            i, j-k, QTableWidgetItem(str(col)))
+                    j = j + 1
+
+                if len(row) == 11:
+                    if col == row[0] or col == row[1] or col == row[3] or col == row[4] or col == row[6] or col == row[7] or col == row[9] or col == row[10]:
+                        k = k + 1
+                    else:
+                        self.tableWidget.setItem(
+                            i, j-k, QTableWidgetItem(str(col)))
+                    j = j + 1
             i = i + 1
 
         header = self.tableWidget.horizontalHeader()
@@ -342,8 +394,6 @@ class MainWindow(QMainWindow):
             j = j+1
 
         self.update()
-
-
 
     def mouseClick(self, lat, lng):
         self.webView.addPointMarker(lat, lng)
